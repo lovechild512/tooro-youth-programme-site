8604414208cc49d90214831ba380ca363d5036e0
lovechild512 <briayo6@gmail.com>
2025-11-05 13:27:06 +0300

    Create RootLayout component with metadata

---
+++ app/app/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Tooro Youth Programme"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
